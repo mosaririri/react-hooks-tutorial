3c5cca83beb868eff8de64042f3c3dabe6926ca5
mosaririri <dos.quoi813@gmail.com>
2020-03-14 16:57:11 +0900

    useStateの使用方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset= () => setCount(0)
+  const devide3 = () => setCount(count % 3 === 0 ? count / 3 : count)
   return (
-    <div>
-      this is a template.
-   </div>
+    <> 
+      <div>
+        count: {count}
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={devide3}>3の倍数のときに3で割る</button>
+
+      </div>
+    </>
+    
   );
 }
 
