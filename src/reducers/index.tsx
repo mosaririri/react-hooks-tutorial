b5b605f7b946cc28bce1d3a9b054b3e38d4166f8
mosaririri <dos.quoi813@gmail.com>
2020-03-26 09:00:00 +0900

    create reducer for events

---
+++ src/reducers/index.tsx
@@ -0,0 +1,23 @@
+type Event = {
+    id: number,
+    title: string,
+    body: string
+}
+
+const events = (state: Event[] = [], action: any) => {
+   switch(action.type) {
+       case 'CREATE_EVENT':
+           const event = { title: action.title, body: action.body }
+           const length = state.length
+           const id = (length === 0) ? 1 : state[length - 1].id + 1 
+           return [ ...state, { id, ...event }] 
+       case 'DELETE_EVENT':
+           return state
+       case 'DELETE_ALL_EVENTS':
+           return []
+       default:
+           return state
+   }
+}
+
+export default events
